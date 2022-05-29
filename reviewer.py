8786ea879d316cab78fba55f55c42ef07561b2bb
leomath42 <leonardomatheus042@gmail.com>
2022-05-29 19:28:08 -0300

    Padronizando o projeto #2

--- reviewer.py
+++ reviewer.py
@@ -5,10 +5,60 @@ import pandas as pd
 # Parse command line arguments
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("-o", "--original_file", help="Name of the original file")
-parser.add_argument("-f", "--filled_file", help="Name of the file with the input plan applied")
-parser.add_argument("-m", "--measure", default="MSE", choices=("MSE"), help="Input measure to be used")
+parser.add_argument(
+    "-f", "--filled_file", help="Name of the file with the input plan applied"
+)
+parser.add_argument(
+    "-m", "--measure", default="MSE", choices=("MSE"), help="Input measure to be used"
+)
 parser.add_argument("-a", "--attribute", help="Name of the column to validate")
 args = vars(parser.parse_args())
+
+
+class AbstractReviewerPlan(ABC):
+    """
+    Abstract base class ImputationPlan.
+    All imputation plans needs inherit from this base class.
+    """
+
+    @abstractmethod
+    def strategy(self, csv: Csv, column_name: str):
+        """
+        Abstract Strategy
+        """
+        raise NotImplementedError()
+
+
+class Mean(AbstractImputationPlan):
+    """
+    Implement a imputation plan strategy.
+    """
+
+    def strategy(self, df: DataFrame, column_name: str) -> DataFrame:
+        ...
+
+
+class Reviewer:
+    def __init__(self) -> None:
+        pass
+
+    def run() -> None:
+        pass
+
+    def save_result(self, output_file: Optional[str] = None) -> None:
+        """_summary_
+
+        Args:
+            name (Optional[str], optional): _description_. Defaults to None.
+
+        Raises:
+            Exception: _description_
+        """
+        if self._result.empty:
+            raise Exception("unhandled exception.")
+
+        self._result.to_csv(self.handle_output_file_name(output_file), index=False)
+
 
 def reviewer_mse(column_original, column_filled):
     """
@@ -17,16 +67,18 @@ def reviewer_mse(column_original, column_filled):
     """
     return mean_squared_error(column_filled, column_original)
 
+
 def execute_validation_mechanism(mechanism, column_original, column_filled):
     """
     Chooses the validation mechanism function to execute base on the input
     Takes both columns to apply the mechanism to
     Returns a call to the mechanism function
     """
-    if (mechanism == "MSE"):
+    if mechanism == "MSE":
         return reviewer_mse(column_original, column_filled)
-    elif (mechanism == "other"):
+    elif mechanism == "other":
         pass
+
 
 def reviewer(original_file, filled_file, column, mechanism):
     """
@@ -37,7 +89,8 @@ def reviewer(original_file, filled_file, column, mechanism):
     original = pd.read_csv(original_file)
     filled = pd.read_csv(filled_file)
     result = execute_validation_mechanism(mechanism, original[column], filled[column])
-    print('Measure:', float(f'{result:.4f}'))
+    print("Measure:", float(f"{result:.4f}"))
+
 
 def main():
     """
@@ -47,7 +100,8 @@ def main():
     filled_file = args["filled_file"]
     column = args["attribute"]
     mechanism = args["measure"]
-    reviewer(original_file, filled_file, column, mechanism) 
+    reviewer(original_file, filled_file, column, mechanism)
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     main()
